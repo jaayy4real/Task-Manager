d2472100618ec979e4d0a5fd9fa2021bcb9cfca8
jaay4real <ekpojeffrey@gmail.com>
2024-05-06 09:39:04 +0100

    third commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require("express-session");
+const authRoutes = require('./routes/authentication.route');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret:'secret',
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use("/api/auth", authRoutes);
 
 mongoose.connect("mongodb+srv://ekpojeffrey:mary4jeffrey@taskmanager.vgvdnsz.mongodb.net/TaskManager?retryWrites=true&w=majority&appName=TaskManager")
 .then(() => {
---
+++ routes/authentication.route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { registration, login, logout } = require('../controllers/authentication.controller.js')
+
+
+router.post('/register', registration)
+router.post('/login', login)
+router.post('/logout', logout)
+
+
+module.exports = router
